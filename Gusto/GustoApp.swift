9658e35bcf69cc4cb7545c580e0f3718d500f4ac
Chris Ameter <29186822+ameter@users.noreply.github.com>
2023-06-27 11:36:42 -0600

    Initial Commit

---
+++ Gusto/GustoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GustoApp.swift
+//  Gusto
+//
+//  Created by Chris Ameter on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct GustoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
